05106f9c4a36ea41eff39a8e350e869923ee590f
Tom O'Brien <tom-ob@likely.co>
2014-01-16 18:28:28 +0000

    Refactor some scraping code - still needs tidying

--- src/markov/scrape.clj
+++ src/markov/scrape.clj
@@ -9,6 +9,18 @@
 (defn url->hickory-response [url & {:as params}]
   (as-hickory (parse (:body (http/get url params)))))
 
+(defn parse-text [story]
+  (letfn [(parse-element [element]
+            (cond
+             (:content element) (parse-text (:content element))
+             (string? element) element
+             (coll? element) (clojure.string/join " " (map parse-text element))))]
+    (str/replace (str/trim (str/join "\n"
+                                     (map parse-element
+                                          story)))
+                 #"\s+"
+                 " ")))
+
 (def fanfiction-net-url
   "https://www.fanfiction.net")
 
@@ -29,6 +41,8 @@
 #_(map (comp (partial re-find #"(?<=/s/)\d+") :href :attrs)
      (s/select (s/child (s/class :stitle))
                (fanfiction-net-search "book/Lord-of-the-Rings")))
+
+
 
 ;; Stories
 
@@ -54,3 +68,13 @@
                    (:content (first (s/select (s/id :chap_select) hickory-response)))))))
 
 #_(story "9983730")
+
+;; Guardian
+
+#_(def guardian-test-response
+  (url->hickory-response "http://www.theguardian.com/education/2014/jan/16/student-opportunity-fund-cut-decision"))
+
+#_(def guardian-discussion
+  (url->hickory-response "http://discussion.theguardian.com/discussion/p/3yqk7?commentpage=1&orderby=oldest&threads=collapsed&tab=picks&iframe=true&noposting=true"))
+
+
